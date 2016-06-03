013ec90df13f3d0d3525ca07e666e2788c8ef472
Life is Game <nobl11@bigmir.net>
2016-06-03 22:56:46 +0300

    add amount

---
+++ src/com/goit/modul6/Instruments.java
@@ -0,0 +1,14 @@
+package com.goit.modul6;
+
+public class Instruments {
+    private String name;
+    private int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
