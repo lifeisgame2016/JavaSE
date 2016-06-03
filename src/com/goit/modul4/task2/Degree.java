1d9168d378ce72bc52b9dc5e673c795d1e763672
Life is Game <nobl11@bigmir.net>
2016-06-03 19:06:21 +0300

    add exception

--- src/com/goit/modul4/task2/Degree.java
+++ src/com/goit/modul4/task2/Degree.java
@@ -22,23 +22,29 @@ public class Degree {
 
     public void calsiusToFahrenheit(){
         double fahrenheit;
-        fahrenheit = (this.celsius*1.8)+32;
+        fahrenheit = (this.celsius * 1.8) + 32;
         System.out.println(this.celsius + "'C = " + fahrenheit + "'F");
+
     }
 
-    public void fahrenheitToCelsius(){
+    public void fahrenheitToCelsius() {
         double celsius;
-        celsius = (this.fahrenheit-32)/1.8;
+        celsius = (this.fahrenheit - 32) / 1.8;
         System.out.println(this.fahrenheit + "'F = " + celsius + "'C");
     }
 
     public static void main (String[] args) {
         Degree degree = new Degree();
+        try {
+            degree.setCelsius(20);
+            degree.calsiusToFahrenheit();
 
-        degree.setCelsius(20);
-        degree.calsiusToFahrenheit();
-
-        degree.setFahrenheit(212);
-        degree.fahrenheitToCelsius();
+            degree.setFahrenheit(212);
+            degree.fahrenheitToCelsius();
+        } catch(IllegalArgumentException e){
+            System.out.println("You entered the frong number of argument");
+        } catch (Exception e){
+            System.out.println("Thre was stil some exception");
+        }
     }
 }
