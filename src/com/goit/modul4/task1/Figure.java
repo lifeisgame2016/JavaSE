42596e2646ce2ff037ab53dd349872868a36246a
Life is Game <nobl11@bigmir.net>
2016-06-03 20:29:34 +0300

    New class

--- src/com/goit/modul4/task1/Figure.java
+++ src/com/goit/modul4/task1/Figure.java
@@ -2,7 +2,7 @@ package com.goit.modul4.task1;
 
 
 public abstract class Figure {
-    static final double pi = 3.14159;
+    //static final double pi = 3.14159;
     abstract String getName();
     abstract double getArea();
 }
