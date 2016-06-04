9c952861708806f801479ac1ade897d3faa5ae46
Life is Game <nobl11@bigmir.net>
2016-06-04 22:09:29 +0300

    v.2.2

--- src/com/goit/modul4/task2/DegreeConverter.java
+++ src/com/goit/modul4/task2/DegreeConverter.java
@@ -1,24 +1,6 @@
 package com.goit.modul4.task2;
 
 public class DegreeConverter {
-    private  int celsius;
-    private int fahrenheit;
-
-    public int getCelsius() {
-        return celsius;
-    }
-
-    public void setCelsius(int celsius) {
-        this.celsius = celsius;
-    }
-
-    public int getFahrenheit() {
-        return fahrenheit;
-    }
-
-    public void setFahrenheit(int fahrenheit) {
-        this.fahrenheit = fahrenheit;
-    }
 
     public static double calsiusToFahrenheit(double celsius){
         double fahrenheit;
@@ -35,11 +17,11 @@ public class DegreeConverter {
     public static void main (String[] args) {
         DegreeConverter degreeConverter = new DegreeConverter();
 
-        degreeConverter.setCelsius(20);
-        System.out.println(degreeConverter.getCelsius() + "'C = " + degreeConverter.calsiusToFahrenheit(degreeConverter.getCelsius()) + "'F");
+        double celsius = 20;
+        System.out.println(celsius + "'C = " + degreeConverter.calsiusToFahrenheit(celsius) + "'F");
 
-        degreeConverter.setFahrenheit(212);
-        System.out.println(degreeConverter.getFahrenheit() + "'F = " + degreeConverter.fahrenheitToCelsius(degreeConverter.getFahrenheit()) + "'C");
+        double fahrenheit = 212;
+        System.out.println(fahrenheit + "'F = " + degreeConverter.fahrenheitToCelsius(fahrenheit) + "'C");
 
     }
 }
