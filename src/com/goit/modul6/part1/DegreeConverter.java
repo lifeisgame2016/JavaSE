8bc56770be4cb686913ecf963167a68f4a7a0696
Life is Game <nobl11@bigmir.net>
2016-06-11 19:35:12 +0300

    1

---
+++ src/com/goit/modul6/part1/DegreeConverter.java
@@ -0,0 +1,32 @@
+package com.goit.modul6.part1;
+
+import java.io.IOException;
+
+public class DegreeConverter {
+
+    public static final double FREEZING_POINT = 32.0;
+    public static final double DISTANCE_POINT = 1.8;
+
+    public static double calsiusToFahrenheit(double celsius){
+return (celsius * DISTANCE_POINT) + FREEZING_POINT;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - FREEZING_POINT) / DISTANCE_POINT;
+    }
+
+    public static void main (String[] args) {
+        DegreeConverter degreeConverter = new DegreeConverter();
+
+        try{
+        double celsius = 20;
+        System.out.println(celsius + "'C = " + degreeConverter.calsiusToFahrenheit(celsius) + "'F");
+
+        double fahrenheit = 212;
+        System.out.println(fahrenheit + "'F = " + degreeConverter.fahrenheitToCelsius(Double.valueOf("212r")) + "'C");
+        } catch (Exception e){
+            System.out.println(e.fillInStackTrace());
+        }
+
+    }
+}
