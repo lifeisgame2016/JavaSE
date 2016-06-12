0655200e47faf52af676396430c660667f44fdf4
Life is Game <nobl11@bigmir.net>
2016-06-12 23:08:49 +0300

    1.2

--- src/com/goit/modul6/part2/MusicShop.java
+++ src/com/goit/modul6/part2/MusicShop.java
@@ -23,12 +23,47 @@ public class MusicShop {
         for (int i = 0; i < count; i++){
             listInstruments.add(instruments[random.nextInt(instruments.length)]);
         }
-
-        System.out.println(listInstruments.toString()+"\n");
-
     }
 
-    public List<Instrument> prepareInstruments (Map<Instrument, Integer> oreder){
+    public HashMap<Instrument,Integer> countInstrument(){
+        HashMap<Instrument, Integer> result = new HashMap<Instrument, Integer>();
+
+        for (Instrument instrument : listInstruments){
+            boolean isExist = false;
+            for (Map.Entry<Instrument, Integer> pair : result.entrySet()){
+                if (pair.getKey().equals(instrument)){
+                    result.put(instrument,result.get(instrument)+1);
+                    isExist = true;
+                }
+            }
+            if (!isExist){
+                result.put(instrument,1);
+            }
+        }
+        return result;
+    }
+
+
+    public void printShop(HashMap<Instrument,Integer> map){
+        for(Map.Entry<Instrument,Integer> pair : map.entrySet()){
+            System.out.print( pair.getKey().getName() + " - " + pair.getValue() + ";\t");
+        }
+        System.out.println();
+    }
+
+    public List<Instrument> prepareInstruments (Map<Instrument, Integer> order) throws WrongInstrumentException {
+
+        /*for (Map.Entry<Instrument, Integer> pair : order.entrySet()){
+            switch (pair.getKey().getName()){
+                case "guitar": null;
+                    break;
+                case "piano": null;
+                    break;
+                case "trumpet": null;
+                    break;
+                default: throw new WrongInstrumentException(pair.getKey().getName());
+            }
+        } */
         return null;
     }
 
--- src/com/goit/modul6/part2/Test.java
+++ src/com/goit/modul6/part2/Test.java
@@ -1,10 +1,22 @@
 package com.goit.modul6.part2;
 
+import java.util.HashMap;
+
 public class Test {
 
     public static void main(String[] args){
         MusicShop musicShop = new MusicShop();
 
         musicShop.importInstrument(10);
+
+        HashMap<Instrument, Integer> map = new HashMap<>();
+
+        map = musicShop.countInstrument();
+
+        musicShop.printShop(map);
+
+        System.out.println(musicShop.getListInstruments());
+
+
     }
 }
