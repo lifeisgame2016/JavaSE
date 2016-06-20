831faa8c31a0fcc22ba51ed9d3810519c35b7532
Life is Game <nobl11@bigmir.net>
2016-06-20 23:56:45 +0300

    beta 1

---
+++ src/com/goit/modul9/CipherCaesar.java
@@ -0,0 +1,44 @@
+package com.goit.modul9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CipherCaesar {
+
+    List<Character> alphabet = new ArrayList<Character>();
+    private final static char[] PUNCTUATION = {',','.',';',':','!','?',' ','-','@'};
+
+    public CipherCaesar(){
+        for(char c = 'A'; c <= 'z'; c++){
+            alphabet.add(c);
+            if(c == 'Z'){
+                c = 'a' - 1;
+            }
+        }
+        for(char c = '0'; c <= '9'; c++){
+            alphabet.add(c);
+        }
+        for(char c : PUNCTUATION){
+            alphabet.add(c);
+        }
+    }
+
+    public String encrypt(String value, int key){
+        int sizeAlph = alphabet.size();
+        char[] valustChr = value.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for(char c : valustChr){
+            if (alphabet.indexOf(c) != -1) result.append(alphabet.get((alphabet.indexOf(c) + key) % sizeAlph));
+            else result.append(c);
+        }
+        return result.toString();
+    }
+
+
+//////////////////////////////////////////////////////////
+    public static void main(String[] args) {
+        CipherCaesar newValue = new CipherCaesar();
+        System.out.println(newValue.encrypt("abcd",4));
+    }
+
+}
